a03cd4b53c9129ff5f453ade6e631a982df09f5f
tomas Solano Sanchez <tomas.solano.sanchez@gmail.com>
2021-08-13 13:59:30 -0300

    1.0.4

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,26 @@
+from .models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class UserAdminConfig(UserAdmin):
+    model = User
+    search_fields = ('email', 'first_name',)
+    list_filter = ('email', 'is_active', 'is_staff')
+    ordering = ('-start_date',)
+    list_display = ('email', 'id', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('password', 'email')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+        ('Personal', {'fields': ('about',)}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'user_name', 'first_name','last_name', 'password1', 'password2', 'is_active', 'is_staff')}
+         ),
+    )
+
+
+admin.site.register(User, UserAdminConfig)
